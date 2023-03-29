185439bc2c5eecf551310834bd8e41bff9bbfedc
samuelsormin <christlegacies@gmail.com>
2023-03-29 14:47:59 +0700

    fix sidebar hidden on desktop site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,12 @@ export default function Header(props: HeaderProps) {
                     </svg>
                     <span className="hidden sm:block text-red-500"><strong>Logout</strong></span>
                 </button>
-                <button onClick={() => props.onClick(true)}>
+                <button className="xl:hidden" onClick={() => props.onClick(true)}>
+                    <svg width="25" height="20" viewBox="0 0 25 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 8.57143H25V11.4286H0V8.57143ZM0 0H25V2.85714H0V0ZM0 17.1429H25V20H0V17.1429Z" fill="#323A46" />
+                    </svg>
+                </button>
+                <button className="hidden xl:block">
                     <svg width="25" height="20" viewBox="0 0 25 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 8.57143H25V11.4286H0V8.57143ZM0 0H25V2.85714H0V0ZM0 17.1429H25V20H0V17.1429Z" fill="#323A46" />
                     </svg>
